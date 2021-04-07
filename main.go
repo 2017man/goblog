998cf778a03550378c19bf71fd78bf7e1dab58dd
满向向 <3398284534@qq.com>
2021-04-07 16:39:34 +0800

    尝试http.ServeMux

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html;charset=utf-8")
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")	
+		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
@@ -16,16 +16,27 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func aboutHandler(w http.ResponseWriter, r *http.Request)  {
-	w.Header().Set("Content-Type","text/html;charset=utf-8")
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html;charset=utf-8")
 	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
-	"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 }
 
 func main() {
+	router := http.NewServeMux()
 	// 主页
-	http.HandleFunc("/", defaultHandler)
+	router.HandleFunc("/", defaultHandler)
 	// 关于页
-	http.HandleFunc("/about", aboutHandler)
-	http.ListenAndServe(":4000", nil)
+	// 列表 or 创建
+    router.HandleFunc("/articles", func(w http.ResponseWriter, r *http.Request) {
+        switch r.Method {
+        case "GET":
+            fmt.Fprint(w, "访问文章列表")
+        case "POST":
+            fmt.Fprint(w, "创建新的文章")
+        }
+    })
+	// 关于页
+	router.HandleFunc("/about", aboutHandler)
+	http.ListenAndServe(":4000", router)
 }
